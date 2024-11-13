60c9dfd082d6f63e08e93d622bace062288521e8
Malak-Amr-Elsadek <malakelsadek.2015@gmail.com>
2024-11-13 16:47:52 +0300

    Age check

---
+++ main.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age:"))
+print("Type Yes or No ")
+adult=int(input("Do you have a adult:"))
+
+if age<20:
+    print("Allowed")
+elif adult=="Yes":
+    print("Allowed")
+else:
+    print("Not Allowed")
